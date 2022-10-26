ced42e8e4bc7b5f0476d233d23b3a1ee5bcb7b39
Eduard Tudorache <edy_edytza2003@yahoo.com>
2022-10-26 19:56:37 +0200

    ticket creation working

--- src/screens/dashboard/sidescreen/market/tickets/TicketSchema.jsx
+++ src/screens/dashboard/sidescreen/market/tickets/TicketSchema.jsx
@@ -1,5 +1,16 @@
 import * as Yup from 'yup';
+import ERRORS from '../../../../../constants/formErrors';
 
 const TicketSchema = Yup.object().shape({
-    
+    price: Yup
+        .number()
+        .required(ERRORS.ERROR_REQUIRED)
+        .min(0.13)
+        .max(0.17),
+    quantity: Yup
+        .number()
+        .required(ERRORS.ERROR_REQUIRED)
+        .positive(),
 })
+
+export default TicketSchema
