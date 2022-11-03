12f07885ae92e884558ef857f85ad4521bcefe6c
Eduard Tudorache <edy_edytza2003@yahoo.com>
2022-11-03 21:14:13 +0100

    new notification component not working, import

---
+++ src/components/topbar/notifications/NotificationsWidget.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useNotificationCenter } from "react-toastify/addons/useNotificationCenter"
+
+
+
+const NotificationsWidget = () => {
+  const { notifications } = useNotificationCenter
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NotificationsWidget;
